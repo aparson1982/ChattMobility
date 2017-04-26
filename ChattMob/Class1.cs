27a723423901be29a4b2c7d23bda1216ddc16f5a
aparson1982 <aparson1982@gmail.com>
2017-04-26 14:01:09 -0400

    Added count, backup, and list methods

--- ChattMob/Class1.cs
+++ ChattMob/Class1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -109,19 +111,65 @@ namespace ChattMob
             }
         }
 
-        //public List <string> [] Select()
-        //{
-            
-        //}
+        
+        public int Count()
+        {
+            string query = "SELECT Count(*) FROM customer_table";
+            int Count = -1;
 
-        //public int Count()
-        //{
-            
-        //}
+            if (this.OpenConnection()==true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+                this.CloseConnection();
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
 
         public void Backup()
         {
-            
+            try
+            {
+                DateTime Time = DateTime.Now;
+                int year = Time.Year;
+                int month = Time.Month;
+                int day = Time.Day;
+                int hour = Time.Hour;
+                int minute = Time.Minute;
+                int second = Time.Second;
+                int millisecond = Time.Millisecond;
+
+                string path;
+
+                path = "C:\\MySqlBackup" + year + "-" + month + "-" + day + "-" + hour + "-" + minute + "-" + second +
+                       "-" + millisecond + "-" + ".sql";
+
+                StreamWriter file = new StreamWriter(path);
+
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "mysqldump";
+                psi.RedirectStandardInput = false;
+                psi.RedirectStandardOutput = true;
+                psi.Arguments = string.Format(@"-u{0} -p{1} -h{2} {3}", uid, password, server, database);
+                psi.UseShellExecute = false;
+
+                Process process = Process.Start(psi);
+
+                string output;
+                output = process.StandardOutput.ReadToEnd();
+                file.WriteLine(output);
+                process.WaitForExit();
+                file.Close();
+                process.Close();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Error, unable to backup!");
+            }
         }
 
         public void Restore()
@@ -131,7 +179,38 @@ namespace ChattMob
 
         public List <string> [] Select()
         {
-            
+            string query = "SELECT * FROM customer_table";
+            List<string>[] list = new List<string>[6];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+            list[3] = new List<string>();
+            list[4] = new List<string>();
+            list[5] = new List<string>();
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["CUSTOMER_ID"] + "");
+                    list[1].Add(dataReader["FIRST_NAME"] + "");
+                    list[2].Add(dataReader["LAST_NAME"] + "");
+                    list[3].Add(dataReader["EMAIL"] + "");
+                    list[4].Add(dataReader["PHONE"] + "");
+                    list[5].Add(dataReader["DATE_CREATED"] + "");
+
+                }
+                dataReader.Close();
+                this.CloseConnection();
+                return list;
+            }
+            else
+            {
+                return list;
+            }
         }
 
     }
